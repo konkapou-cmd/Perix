86616595ba4165c21e6467e623335b3a71a6553c
Konstantinos <konkapou@gmail.com>
2026-08-28 18:29:30 +0200

    fix: remove inner border on Your Ad upload card (double line)

--- components/home/CityAdCircles.tsx
+++ components/home/CityAdCircles.tsx
@@ -248,8 +248,6 @@ const styles = StyleSheet.create({
   uploadCardContent: {
     width: CARD_WIDTH,
     height: CARD_IMAGE_HEIGHT,
-    borderWidth: 1.5,
-    borderColor: "#59ABE3",
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: COLORS.backgroundPage,
