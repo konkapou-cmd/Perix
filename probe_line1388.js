a24289a1cd40f0f43de0daa802bc3f75320a0586
Konstantinos <konkapou@gmail.com>
2026-09-04 19:58:27 +0200

    Remove debug script

--- probe_line1388.js
+++
@@ -1,7 +0,0 @@
-const fs = require("fs");
-const path = "dist/_expo/static/js/web";
-const f = fs.readdirSync(path).find((x) => x.startsWith("entry"));
-const s = fs.readFileSync(path + "/" + f, "utf8");
-const lines = s.split("\n");
-console.log("line 1388 length:", lines[1387].length);
-console.log(lines[1387].slice(0, 3000));
